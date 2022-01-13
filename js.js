403002ce4412198027ea10c612f27025e099097f
Dat <trandat251111@gmail.com>
2022-01-13 17:36:32 +0700

    add1

---
+++ js.js
@@ -0,0 +1,10 @@
+let answer = confirm("Shall we play a game boii?");
+function rep(n){
+    if(n){
+        alert("Smart choice. 10pm at my house.")
+    }
+    else {
+        alert("U'll regret that!")
+    }
+}
+rep(answer);
